ff109cd9eff850c20360d9c7d9865405d1017163
FreshSangchoo <157272518+FreshSangchoo@users.noreply.github.com>
2024-09-02 08:08:51 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 988 ms, Memory: 180568 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -31,20 +31,20 @@ public class Main {
     }
     static int left(int find){
         int left = 0;
-        int right = N;
-        while (left < right) {
+        int right = N-1;
+        while (left <= right) {
             int mid = (left + right) / 2;
-            if (find<=cards[mid]) right = mid;
+            if (find<=cards[mid]) right = mid-1;
             else left = mid + 1;
         }
         return left;
     }
     static int right(int find){
         int left = 0;
-        int right = N;
-        while (left < right) {
+        int right = N-1;
+        while (left <= right) {
             int mid = (left + right) / 2;
-            if (find<cards[mid]) right = mid;
+            if (find<cards[mid]) right = mid-1;
             else left = mid + 1;
         }
         return left;
