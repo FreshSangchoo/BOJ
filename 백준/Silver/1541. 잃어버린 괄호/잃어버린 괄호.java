3fc497a656f864fa34944a86e1c7e01e7cde6071
FreshSangchoo <157272518+FreshSangchoo@users.noreply.github.com>
2024-10-27 17:36:07 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 68 ms, Memory: 11716 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -2,20 +2,21 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws Exception{
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st=new StringTokenizer(br.readLine(), "-");
-		int ans=0;
-		int first=-1;
-		while(st.hasMoreTokens()) {
-			int temp=0;
-			StringTokenizer st2=new StringTokenizer(st.nextToken(), "+");
-			while(st2.hasMoreTokens()) {
-				temp+=Integer.parseInt(st2.nextToken());
-			}
-			if(first==-1) first=temp;
-			else ans-=temp;
-		}
-		System.out.println(first+ans);
-	}
+    public static void main(String[] args) throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        // - 에서 끊어서 읽기 = 끊은 부분들을 모두 음수처리해서 더해주면 됨
+        StringTokenizer st=new StringTokenizer(br.readLine(), "-");
+        int ans=0;
+        int first=-1;
+        while(st.hasMoreTokens()) {
+            int temp=0;
+            StringTokenizer st2=new StringTokenizer(st.nextToken(), "+");
+            while(st2.hasMoreTokens()) {
+                temp+=Integer.parseInt(st2.nextToken());
+            }
+            if(first==-1) first=temp;
+            else ans-=temp;
+        }
+        System.out.println(first+ans);
+    }
 }
