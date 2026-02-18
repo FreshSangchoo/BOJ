71789f802efe8cee2e048a14e3d9b9cab21646d6
FreshSangchoo <157272518+FreshSangchoo@users.noreply.github.com>
2026-02-18 12:13:13 +0900

    [Silver III] Title: 안테나, Time: 32 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/18310. 안테나/안테나.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    vector<int> v(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+
+    int middle = v[(N - 1) / 2];
+
+	cout << middle << "\n";
+}
